84db563b7949336a9a5ee310a45daa354572b754
nepp-plus <nepp.plus@gmail.com>
2021-11-13 11:43:15 +0900

    var / val 변수 만드는 용법 차이 체험

--- app/src/main/java/com/neppplus/kotlingrammar_20211113/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlingrammar_20211113/MainActivity.kt
@@ -2,10 +2,32 @@ package com.neppplus.kotlingrammar_20211113
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnVariable01.setOnClickListener {
+
+//            이 안에 적는 코드 - 변수 문법 연습 01
+
+            var myName : String
+
+            myName = "조경진"
+            myName = "김경진"
+
+            Log.d("문법연습01",  myName)
+
+            val friendName : String
+            friendName = "김갑생"
+//            friendName = "김친구"
+
+            Log.d("문법연습01", friendName)
+
+        }
+
     }
 }
