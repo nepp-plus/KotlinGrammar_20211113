0916bdcea6b7219fca27b52e09fa6d4d2f938512
nepp-plus <nepp.plus@gmail.com>
2021-11-13 12:18:58 +0900

    조건문 연습 1 - if /else if / else 체험

--- app/src/main/java/com/neppplus/kotlingrammar_20211113/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlingrammar_20211113/MainActivity.kt
@@ -43,5 +43,32 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnCondition01.setOnClickListener {
+//            조건문 01 버튼 눌리면 실행될 코드
+
+            val userAge = 10
+
+//            나이가 20살 이상인가? -> 로그로 "성인입니다." 문장 출력
+            if (userAge >= 20) {
+
+                Log.d("조건문01", "성인입니다.")
+
+            }
+            else if (userAge >= 17) {
+
+//                성인이 아니고, 질문 -> 17살이상인가?
+                Log.d("조건문01", "고등학생입니다.")
+
+            }
+            else if (userAge >= 14) {
+//                성인이 아니고, 고등학생 도 아닐때 질문 -> 14살 이상인가?
+                Log.d("조건문01", "중학생입니다.")
+            }
+            else {
+                Log.d("조건문01", "초등학생이거나, 미취학아동입니다.")
+            }
+
+        }
+
     }
 }
